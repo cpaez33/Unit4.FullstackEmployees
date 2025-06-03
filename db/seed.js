9b623ed5f1d28644f0cec518d67fc38c9524393b
Christopher Paez <paez.christopher@gmail.com>
2025-06-02 22:23:20 -0400

    Finish Fullstack Employees 1

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,8 @@
+import { faker } from "@faker-js/faker";
+
 import db from "#db/client";
+
+import { createEmployee } from "#db/queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +10,14 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.firstName(),
+      birthday: faker.date.past({ years: 50 }),
+      salary: faker.number.int({ min: 40000, max: 125000 }),
+    };
+    // console.log(employee);
+    await createEmployee(employee);
+  }
   // TODO
 }
